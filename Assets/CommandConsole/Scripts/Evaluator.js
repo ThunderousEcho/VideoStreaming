3ad3636f85415460de3da3996d48c7904b680e3c
ThunderousEcho <thunderousecho@gmail.com>
2017-11-27 15:38:27 -0500

    Implemented iOS ReplayKit.

--- Assets/CommandConsole/Scripts/Evaluator.js
+++ Assets/CommandConsole/Scripts/Evaluator.js
@@ -1,3 +1,13 @@
 ﻿function evaluateNonStrict(code: String) {
+
+#if UNITY_IOS
+
+    return "JS eval not supported on iOS. Use C# instead."
+
+#else
+
     return eval(code);
+
+#endif
+
 }
---
+++ Assets/VideoStreaming/ReplayKitHelper.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_IOS
+using UnityEngine.iOS;
+using UnityEngine.Apple.ReplayKit;
+#endif
+
+public class ReplayKitHelper : MonoBehaviour {
+
+    public Button button;
+    public Text text;
+
+    void Start () {
+#if UNITY_IOS
+        button.interactable = ReplayKit.APIAvailable;
+        if (!button.interactable)
+            text.text = "ReplayKit unavailable.";
+#else
+        button.interactable = false;
+        text.text = "ReplayKit unavailable: not an iOS device.";
+#endif
+    }
+	
+	void OnBroadcastButtonPressed () {
+#if UNITY_IOS
+        button.interactable = ReplayKit.APIAvailable;
+        if (!button.interactable)
+            text.text = "ReplayKit unavailable.";
+        else{
+		    if (ReplayKit.isBroadcasting) {
+                ReplayKit.StopBroadcasting();
+                text.text = "Start broadcasting";
+            } else {
+                ReplayKit.StartBroadcasting((bool success, string error) => Debug.Log(string.Format("Start : {0}, error : `{1}`", success, error)));
+                text.text = "Stop broadcasting";
+            }
+        }
+#endif
+    }
+
+    void Update() {
+#if UNITY_IOS
+        button.interactable = ReplayKit.APIAvailable;
+        if (!button.interactable)
+            text.text = "ReplayKit unavailable.";
+        else{
+            if (ReplayKit.isBroadcasting && !text.text.Equals("Start broadcasting")) {
+                text.text = "Start broadcasting";
+            } else if (!ReplayKit.isBroadcasting && !text.text.Equals("Stop broadcasting")) {
+                text.text = "Stop broadcasting";
+            }
+        }
+#endif
+    }
+}
+
+
